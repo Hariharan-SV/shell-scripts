f01b757fe0b49087022e0df8a1c79b6e42e9f134
HariharanSV <46956177+Hariharan20@users.noreply.github.com>
2020-01-31 07:43:12 +0530

    Create awk.sh

---
+++ awk.sh
@@ -0,0 +1,28 @@
+Insert:
+Use same echo $var>>filename as it has no insert and follows the same pattern
+
+Search:
+awk -F 'FieldSeperator'  '$x~/PATTERN/ {print $0}' filename
+will print line matching the PATTERN in x th column
+
+awk -F 'FieldSeperator'  '~/PATTERN/ {print $0}' filename
+will print line matching the PATTERN
+
+Search: and Replace:
+awk -F 'FieldSeperator'  '$x~/PATTERN/  {gsub(/src/,replacement,$x)}1' filename
+will search for PATTERN in x th column and replaces src by replacement in x th column
+
+Calculate sum of one column based on unique values of another column:
+awk -F 'FieldSeperator' '{a[$x] += $y} END{for (i in a) print i, a[i]}' filename.txt
+searches for unique values of x and calculates sum of yth column
+
+Delete
+awk -F 'FieldSeperator'  '$x!~/PATTERN/ ' filename
+will delete line matching the PATTERN in x th column
+
+Sort:
+sort -tFieldSeperator -kx,xn filename
+will sort n th column of file if it is a number, if it is a text donot use n
+
+
+Any modification made in search and replace or delete will not be stored in original file so send it to the temp file and rename it and reuse it
